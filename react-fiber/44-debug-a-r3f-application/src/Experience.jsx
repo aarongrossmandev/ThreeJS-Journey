99a7e0ebe11847a7df7859952ad151fcbdf64e9f
Aaron <aarongrossman.dev@gmail.com>
2023-05-30 02:43:51 -0400

    leva debug added to r3f app

---
+++ react-fiber/44-debug-a-r3f-application/src/Experience.jsx
@@ -0,0 +1,63 @@
+import { OrbitControls } from "@react-three/drei";
+import { button, useControls } from "leva";
+import { Perf } from "r3f-perf";
+
+export default function Experience() {
+  const { perfVisible } = useControls({
+    perfVisible: true,
+  });
+
+  const { position, color, visible } = useControls("sphere", {
+    position: {
+      value: { x: -2, y: 0 },
+      step: 0.01,
+      joystick: "invertY",
+    },
+    color: "#ff0000",
+    visible: true,
+    myInterval: {
+      min: 0,
+      max: 10,
+      value: [4, 5],
+    },
+    clickMe: button(() => {
+      console.log("ok");
+    }),
+    choice: { options: ["a", "b", "c"] },
+  });
+
+  const { cubeScale } = useControls("cube", {
+    cubeScale: {
+      value: 1.5,
+      step: 0.01,
+      min: 0,
+      max: 5,
+    },
+  });
+
+  return (
+    <>
+      {perfVisible && <Perf position="top-left" />}
+
+      <OrbitControls makeDefault />
+
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <mesh position={[position.x, position.y, 0]} visible={visible}>
+        <sphereGeometry />
+        <meshStandardMaterial color={color} />
+      </mesh>
+
+      <mesh position-x={2} scale={cubeScale}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+    </>
+  );
+}
