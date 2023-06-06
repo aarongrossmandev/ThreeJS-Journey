00f562a9fbca5f96eb58c3b6ad5a185221ca27d3
Aaron <aarongrossman.dev@gmail.com>
2023-06-06 00:42:15 -0400

    post processing section completed

---
+++ react-fiber/49-mouse-events-with-r3f/src/Experience.jsx
@@ -0,0 +1,67 @@
+import { useFrame } from "@react-three/fiber";
+import { meshBounds, OrbitControls, useGLTF } from "@react-three/drei";
+import { useRef } from "react";
+
+export default function Experience() {
+  const cube = useRef();
+  const hamburger = useGLTF("./hamburger.glb");
+
+  useFrame((state, delta) => {
+    cube.current.rotation.y += delta * 0.2;
+  });
+
+  const eventHandler = (event) => {
+    cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`);
+  };
+
+  return (
+    <>
+      <OrbitControls makeDefault />
+
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <mesh
+        position-x={-2}
+        onClick={(event) => {
+          event.stopPropagation();
+        }}
+      >
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh
+        ref={cube}
+        raycast={meshBounds}
+        onClick={eventHandler}
+        onPointerEnter={() => {
+          document.body.style.cursor = "pointer";
+        }}
+        onPointerLeave={() => {
+          document.body.style.cursor = "default";
+        }}
+        position-x={2}
+        scale={1.5}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+
+      <primitive
+        object={hamburger.scene}
+        scale={(0.25, 0.25, 0.25)}
+        position-y={0.5}
+        onClick={(e) => {
+          console.log(e.object.name);
+          e.stopPropagation();
+        }}
+      />
+    </>
+  );
+}
---
+++ react-fiber/50-post-processing-with-r3f/src/Experience.jsx
@@ -0,0 +1,133 @@
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import {
+  SSR,
+  DepthOfField,
+  Bloom,
+  Noise,
+  Glitch,
+  EffectComposer,
+  Vignette,
+} from "@react-three/postprocessing";
+import { BlendFunction, GlitchMode } from "postprocessing";
+import { useControls } from "leva";
+import Drunk from "./Drunk";
+import { useRef } from "react";
+
+export default function Experience() {
+  const drunkRef = useRef();
+
+  //   const ssrProps = useControls("SSR Effect", {
+  //     temporalResolve: true,
+  //     STRETCH_MISSED_RAYS: true,
+  //     USE_MRT: true,
+  //     USE_NORMALMAP: true,
+  //     USE_ROUGHNESSMAP: true,
+  //     ENABLE_JITTERING: true,
+  //     ENABLE_BLUR: true,
+  //     temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+  //     temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+  //     maxSamples: { value: 0, min: 0, max: 1 },
+  //     resolutionScale: { value: 1, min: 0, max: 1 },
+  //     blurMix: { value: 0.5, min: 0, max: 1 },
+  //     blurKernelSize: { value: 8, min: 0, max: 8 },
+  //     blurSharpness: { value: 0.5, min: 0, max: 1 },
+  //     rayStep: { value: 0.3, min: 0, max: 1 },
+  //     intensity: { value: 1, min: 0, max: 5 },
+  //     maxRoughness: { value: 0.1, min: 0, max: 1 },
+  //     jitter: { value: 0.7, min: 0, max: 5 },
+  //     jitterSpread: { value: 0.45, min: 0, max: 1 },
+  //     jitterRough: { value: 0.1, min: 0, max: 1 },
+  //     roughnessFadeOut: { value: 1, min: 0, max: 1 },
+  //     rayFadeOut: { value: 0, min: 0, max: 1 },
+  //     MAX_STEPS: { value: 20, min: 0, max: 20 },
+  //     NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+  //     maxDepthDifference: { value: 3, min: 0, max: 10 },
+  //     maxDepth: { value: 1, min: 0, max: 1 },
+  //     thickness: { value: 10, min: 0, max: 10 },
+  //     ior: { value: 1.45, min: 0, max: 2 },
+  //   });
+
+  const drunkProps = useControls("Drunk Effect", {
+    frequency: { value: 2, min: 1, max: 20 },
+    amplitude: { value: 0.1, min: 0, max: 1 },
+  });
+
+  return (
+    <>
+      <color args={["#ffffff"]} attach="background" />
+
+      {/* multisamping removes stair like effect on pixels, default=8 */}
+      <EffectComposer>
+        {/* Vignette makes the corners of the render darker */}
+        {/* <Vignette
+          offset={0.3}
+          darkness={0.9}
+          blendFunction={BlendFunction.NORMAL}
+        />
+        <Glitch
+          delay={[0.5, 1]}
+          duration={[0.1, 0.3]}
+          strength={[0.2, 0.4]}
+          mode={GlitchMode.CONSTANT_WILD}
+        />
+        <Noise premultiply blendFunction={BlendFunction.SOFT_LIGHT} />
+        <Bloom mipmapBlur intensity={0.1} luminanceThreshold={0} /> */}
+
+        {/* values are 0-1 for distance for focusDistance and focalLength --- not great performance. */}
+        {/* <DepthOfField
+          focusDistance={0.025}
+          focalLength={0.025}
+          bokehScale={6}
+        />
+        <SSR {...ssrProps} /> */}
+        <Drunk
+          ref={drunkRef}
+          {...drunkProps}
+          blendFunction={BlendFunction.MULTIPLY}
+        />
+      </EffectComposer>
+
+      <Perf position="top-left" />
+
+      <OrbitControls makeDefault />
+
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <mesh castShadow position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh castShadow position-x={2} scale={1.5}>
+        <boxGeometry />
+        {/* bloom effect, array of colors for ranges, like a sunlight glow. */}
+        {/* <meshStandardMaterial color={[5, 2, 1]} toneMapped={false} /> */}
+
+        {/* Another way to create the bloom sun glow effect */}
+        {/* <meshStandardMaterial
+          color="white"
+          emissive="orange"
+          emissiveIntensity={4}
+          toneMapped={false}
+        /> */}
+
+        {/* Makes the light uniform around on all sides so as to make a bulb or sunlight effect naturally */}
+        {/* <meshBasicMaterial color={[1.5 * 4, 1 * 4, 4 * 4]} toneMapped={false} /> */}
+
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh
+        receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" metalness={0} roughness={0} />
+      </mesh>
+    </>
+  );
+}
